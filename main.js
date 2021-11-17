0362f4bded0671d4e704b0fa5a22a51108274fe0
mbvDevelop <mbv.design.co@gmail.com>
2021-11-17 15:48:18 +0100

    sixth commit

--- main.js
+++ main.js
@@ -74,5 +74,24 @@ for (item of placesToTravel) {
  
 console.log(placesToTravel)
 
+// Iteracion 6
+ 
+const toys = [
+    {id: 5, name: 'Buzz MyYear'},
+    {id: 11, name: 'Action Woman'},
+    {id: 23, name: 'Barbie Man'},
+    {id: 40, name: 'El gato con Guantes'},
+    {id: 40, name: 'El gato felix'}
+    ]
+ 
+    for (item of toys) {
+        if (item.name.includes("gato")) {
+            toys.splice(toys.indexOf(item), 1)
+        }
+    }
+ 
+    console.log(toys)
+
+
 
 
