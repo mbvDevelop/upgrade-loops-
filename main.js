0239b83ec4eda8e2815d12d566f7a8bf711a97ef
mbvDevelop <mbv.design.co@gmail.com>
2021-11-17 15:47:41 +0100

    fifth commit

--- main.js
+++ main.js
@@ -60,4 +60,19 @@ for (item in alien) {
     console.log(item)
 }
 
+// Iteracion 5
+ 
+const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
+ 
+for (item of placesToTravel) {
+    if (item.id == 11) {
+        placesToTravel.splice(placesToTravel.indexOf(item), 1)
+    } else if (item.id == 40) {
+        placesToTravel.splice(placesToTravel.indexOf(item), 1)
+    }
+}
+ 
+console.log(placesToTravel)
+
+
 
